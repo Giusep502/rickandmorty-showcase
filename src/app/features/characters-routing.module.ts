afd65f477e2e21795c71349e9920579dc87c69ca
Giuseppe Di Francesco <giusep502@gmail.com>
2021-02-24 10:19:48 +0100

    feat: added routing

---
+++ src/app/features/characters-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CharacterListPageComponent } from './characters/containers/character-list-page/character-list-page.component';
+
+const routes: Routes = [
+  {
+    path: ':id',
+    component: CharacterListPageComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CharactersRoutingModule {}
---
+++ src/app/features/characters/containers/character-list-page/character-list-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-character-list-page',
+  templateUrl: './character-list-page.component.html',
+  styleUrls: ['./character-list-page.component.scss'],
+})
+export class CharacterListPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
